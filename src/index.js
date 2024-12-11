8ca2fec9206ecb8b781bf8a7c2f061cf1816535e
Lehlohonlo Botji <lbotji@gmail.com>
2024-12-11 21:56:51 +0200

    injected the classlist romove and innerhtml in the code

--- src/index.js
+++ src/index.js
@@ -12,12 +12,16 @@ new Typewriter("#recipe", {
 function generateRecipe(event) {
   event.preventDefault();
 
-let instructionInputElement = document.querySelector("#user-instuctions");
+let instructionsInputElement = document.querySelector("#user-instructions");
 let apiKey = "dac27f8d3491eb544bc74fd800f1to5c";
-let prompt = `User instuctions: Generate a dessert recipe about ${instructionInput.value}`;
 let context = 
 "You are a world renowned pastry chef. Your mission is to generate a clear and simple recipe in HTML format and separate each line with <br />. Make sure to follow user instuctions. Sign recipe with 'SheCodes AI' inside a <strong></strong> element at the end and not at the beginning. Do not include title.";
+let prompt = `User instructions: Generate a dessert recipe about ${instructionsInput.value}`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let recipeElement = document.querySelector("#recipe");
+recipeElement.classList.remove("hidden")
+recipeElement.innerHTML = `<div class="generating">⏳ Generating your dessert recipe...</div>`;
 
 
 axios.get(apiUrl).then(displayRecipe);
