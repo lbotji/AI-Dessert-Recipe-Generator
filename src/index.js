0d80076c60bda1c9ec4c6044e5bab3d2537e8f11
Lehlohonlo Botji <lbotji@gmail.com>
2024-12-11 13:48:33 +0200

    edited the prompt to structure it better

--- src/index.js
+++ src/index.js
@@ -12,14 +12,13 @@ new Typewriter("#recipe", {
 function generateRecipe(event) {
   event.preventDefault();
 
+let instructionInputElement = document.querySelector("#user-instuctions");
 let apiKey = "dac27f8d3491eb544bc74fd800f1to5c";
-let prompt = `User instuctions: Generate a dessert recipe about ${instuctions.value}`;
+let prompt = `User instuctions: Generate a dessert recipe about ${instructionInput.value}`;
 let context = 
-"You are a world renowned pastry chef. Your mission is to generate a clear and simple recipe in HTML and separate each line with <br />. Make sure to follow user instuctions. Sign recipe with 'SheCodes AI' inside a <strong></strong> element.";
+"You are a world renowned pastry chef. Your mission is to generate a clear and simple recipe in HTML format and separate each line with <br />. Make sure to follow user instuctions. Sign recipe with 'SheCodes AI' inside a <strong></strong> element at the end and not at the beginning. Do not include title.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-let recipeElement = document.querySelector("#recipe");
-recipeElement.innerHTML = "Generating recipe, please wait..."
 
 axios.get(apiUrl).then(displayRecipe);
   
